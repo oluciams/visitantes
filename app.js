d26dddd41b09292b692b5958c5c79643dd754fec
olga <oluciams84@gmail.com>
2021-07-13 15:25:31 -0500

    Modularizando la conexion a la base de datos

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const express = require('express')
-const mongoose = require('mongoose')
+
 const app = express()
 
+require('./configurationdb/configdb')
 
 const Visitor = require('./models/visitor')
 
@@ -30,13 +31,6 @@ app.get('/', async (req, res)=>{
 
 
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { 
-    useNewUrlParser: true
- });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {console.log('db connected')})   
 
 
 
--- configurationdb/configdb.js
+++ configurationdb/configdb.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { 
+    useNewUrlParser: true
+ });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {console.log('db connected')})   
